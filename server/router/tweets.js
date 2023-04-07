4f45dc2d5fe399eb711e56c02959648fb733fb4d
song <55rsearchco@gmail.com>
2023-04-07 12:20:58 +0900

    드위터에 유효성 검사 적용해 보기 💡

--- server/router/tweets.js
+++ server/router/tweets.js
@@ -1,18 +1,28 @@
 import express from "express";
 import "express-async-error";
 import * as tweetsController from "../controller/tweets.js";
+import { body, param } from "express-validator";
+import { validate } from "../middleware/validator.js";
 
 const router = express.Router();
+
+const validateTweet = [
+  body("text")
+    .trim()
+    .isLength({ min: 3 })
+    .withMessage("text should be at least 3 characters"),
+  validate,
+];
 
 router
   .route("/")
   .get(tweetsController.getTweets)
-  .post(tweetsController.createTweet);
+  .post([validateTweet, tweetsController.createTweet]);
 
 router
   .route("/:id")
   .get(tweetsController.getTweet)
-  .put(tweetsController.updateTweet)
+  .put([validateTweet, tweetsController.updateTweet])
   .delete(tweetsController.removeTweet);
 
 export default router;
