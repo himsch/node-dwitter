54bec73cda0a94bcf07900b93492c25af26add3c
song <55rsearchco@gmail.com>
2023-04-10 22:34:26 +0900

    리액트 프론트엔드 가입/로그인 구현 💡

--- client/src/service/tweet.js
+++ client/src/service/tweet.js
@@ -1,32 +1,44 @@
 export default class TweetService {
-  constructor(http) {
+  constructor(http, tokenStorage) {
     this.http = http;
+    this.tokenStorage = tokenStorage;
   }
 
   async getTweets(username) {
     const query = username ? `?username=${username}` : "";
     return this.http.fetch(`/tweets${query}`, {
       method: "GET",
+      headers: this.getHeader(),
     });
   }
 
   async postTweet(text) {
     return this.http.fetch(`/tweets`, {
       method: "POST",
-      body: JSON.stringify({ text, username: "ellie", name: "Ellie" }),
+      headers: this.getHeader(),
+      body: JSON.stringify({ text }),
     });
   }
 
   async deleteTweet(tweetId) {
     return this.http.fetch(`/tweets/${tweetId}`, {
       method: "DELETE",
+      headers: this.getHeader(),
     });
   }
 
   async updateTweet(tweetId, text) {
     return this.http.fetch(`/tweets/${tweetId}`, {
       method: "PUT",
+      headers: this.getHeader(),
       body: JSON.stringify({ text }),
     });
+  }
+
+  getHeader() {
+    const token = this.tokenStorage.getToken();
+    return {
+      Authorization: `Bearer ${token}`,
+    };
   }
 }
